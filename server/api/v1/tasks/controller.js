88086c718331a33c2061cb12882b74d67f8c18f2
AngelGLlamas <angelgllamas@gmail.com>
2021-04-19 16:34:54 +0200

    Create API layout for Tasks

---
+++ server/api/v1/tasks/controller.js
@@ -0,0 +1,19 @@
+exports.create = (req, rest, next) => {
+  res.json({});
+};
+
+exports.all = (req, rest, next) => {
+  res.json({});
+};
+
+exports.read = (req, rest, next) => {
+  res.json({});
+};
+
+exports.update = (req, rest, next) => {
+  res.json({});
+};
+
+exports.delete = (req, rest, next) => {
+  res.json({});
+};
